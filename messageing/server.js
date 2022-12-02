a051020ecbcef0329a61e4a84ac660205beb6e2d
dca <103521110+DCAus-code@users.noreply.github.com>
2022-12-01 22:17:01 -0500

    Add files via upload

---
+++ messageing/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+// Stream Chat server SDK
+const StreamChat = require('stream-chat').StreamChat;
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+app.listen(8800, () => {
+  console.log('Example app listening on port 8800!');
+});
+
+
+// [...]
+
+const serverClient = new StreamChat('8drx4ww3gjsx', 'xdpezryetxkt56837qzdwnhnngfsa3dmkwpygahevbju6xdcyrxney6zgw2x9dat');
+
+app.get('/token', (req, res) => {
+  const { username } = req.query;
+  if (username) {
+    const token = serverClient.createToken(username);
+    res.status(200).json({ token, status: 'sucess' });
+  } else {
+    res.status(401).json({ message: 'invalid request', status: 'error' });
+  }
+});
+
+// [...]
