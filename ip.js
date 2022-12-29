4fe0c02cb54fa5a0482ab75b4f4f090c8cb7b39d
dca <103521110+DCAus-code@users.noreply.github.com>
2022-12-28 21:15:00 -0500

    Update ip.js

--- ip.js
+++ ip.js
@@ -4,3 +4,11 @@ const dbx = new Dropbox({
   accessToken: const apiKey = process.env.DROPBOX_ID;
 ,
 });
+dbx.filesDownload({ path: '/sire/ip.js' })
+  .then((response) => {
+    // The file contents are in the `fileBinary` field of the response
+    const fileContents = response.fileBinary;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
